3f6263bed8ad5d4d366d1ba1ee9fc63d31495ff0
Ricardo <ricardosr.mota@gmail.com>
2017-09-18 21:07:29 -0300

    Alteracoes

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from agenda.models import Agenda
+from agenda.models import AgendaPrivada
+from agenda.models import AgendaInstituicao
+from agenda.models import UsuarioAgenda
+from agenda.models import Usuario
+from agenda.models import UsuarioAgendaPrivada
 
+
+admin.site.register(Agenda)
+admin.site.register(AgendaPrivada)
+admin.site.register(AgendaInstituicao)
+admin.site.register(Usuario)
+admin.site.register(UsuarioAgenda)
+admin.site.register(UsuarioAgendaPrivada)
 # Register your models here.
