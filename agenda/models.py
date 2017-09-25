6f8dc9a48ed4d9d12ba7e2f902618597dd1c9bb5
Ricardo <ricardosr.mota@gmail.com>
2017-09-25 20:54:41 -0300

    correção prova

--- agenda/models.py
+++ agenda/models.py
@@ -5,20 +5,11 @@ from django.db import models
 class Agenda(models.Model):
     compromisso = models.DateField(blank=True, null=True)
     descricao = models.TextField()
+    privado = models.BooleanField()
+    institucional = models.BooleanField()
 
     def __str__(self):
         return self.descricao
-
-
-class AgendaPrivada(Agenda):
-    descricaoPrivada = models.TextField()
-
-    def __str__(self):
-        return self.descricao
-
-
-class AgendaInstituicao(Agenda):
-    feriados = models.CharField(max_length=128,blank=False, null=False)
 
 class Usuario(models.Model):
     iduser = models.CharField(max_length=128,blank=False, null=False)
@@ -27,12 +18,9 @@ class Usuario(models.Model):
     def __str__(self):
         return self.nome
 
-  
-
 class UsuarioAgenda(models.Model):
-    nome = models.ForeignKey(Usuario, null=True, blank=True)
-    compromisso = models.ForeignKey(Agenda, null=True, blank=True)
+    nome = models.ForeignKey(Usuario, null=True, blank=True, related_name='NomeUsuario')
+    compromisso = models.ForeignKey(Agenda, null=True, blank=True, related_name='compromissoUsuario')
+    convidados = models.ManyToManyField('Usuario')
 
 
-class UsuarioAgendaPrivada(UsuarioAgenda):
-    descricao = models.TextField()
